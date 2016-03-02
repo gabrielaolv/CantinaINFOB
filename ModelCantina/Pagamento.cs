675a024294be8727ea159fded680ccb3b9577a4d
andrealmeida <andre.almeida@escolar.ifrn.edu.br>
2016-03-02 00:41:01 -0300

    V1.1
    Versão inicial do Projeto da Turma INFOB

---
+++ ModelCantina/Pagamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelCantina
+{
+    public class Pagamento
+    {
+        public int IDPagamento {get;set;}
+        public DateTime DataPagamento {get;set;}
+        public double ValorPagamento {get;set;}
+        public Conta Conta { get; set; }
+
+    }
+}
---
+++ ModelCantina/VendaProduto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelCantina
+{
+    public class VendaProduto
+    {
+        public Produto Produto { get; set; }
+        public Venda Venda { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
